bc20c9e5a44c748ac3b75a308fa78eb795e0c7b8
Hitman666 <nikola.breznjak@gmail.com>
2015-06-01 08:48:28 +0200

    production polishing

---
+++ config/env/production.js
@@ -0,0 +1,16 @@
+var port = 1337;
+
+module.exports = {
+	port: port,
+	db: 'mongodb://localhost/todos',
+	facebook: {
+		clientID: '1671331279763296',
+		clientSecret: '4e5ad533fc982e29ac55f87cae6e1484',
+		callbackURL: 'http://localhost:'+ port +'/oauth/facebook/callback'
+	},
+	twitter: {
+		clientID: 'wYilkwNnzhikd7dkdbmU9a34I',
+		clientSecret: 'l766h2tbK1sBhbjfnm4xc089MyrfVFgj8kBSEVrY5ONhrYgxj8',
+		callbackURL: 'http://localhost:'+ port +'/oauth/twitter/callback'
+	}
+};
