b422836e9d0a95c4483f442b495b721c9c6cd934
Nikola Brežnjak <nikola.breznjak@gmail.com>
2015-06-02 03:18:26 -0400

    le production settings once the domain propagated

--- config/env/production.js
+++ config/env/production.js
@@ -6,11 +6,11 @@ module.exports = {
 	facebook: {
 		clientID: '1671331279763296',
 		clientSecret: '4e5ad533fc982e29ac55f87cae6e1484',
-		callbackURL: 'http://178.62.109.116:'+ port +'/oauth/facebook/callback'
+		callbackURL: 'http://meantodo.com/oauth/facebook/callback'
 	},
 	twitter: {
 		clientID: 'wYilkwNnzhikd7dkdbmU9a34I',
 		clientSecret: 'l766h2tbK1sBhbjfnm4xc089MyrfVFgj8kBSEVrY5ONhrYgxj8',
-		callbackURL: 'http://178.62.109.116:'+ port +'/oauth/twitter/callback'
+		callbackURL: 'http://meantodo.com/oauth/twitter/callback'
 	}
 };
